08c22352d885d9cf17cb26146a6af7868e01345c
yamao-sys <yamaopanku@gmail.com>
2024-12-11 10:32:44 +0900

    ESLintのeslint-config-prettier追加

--- frontend/src/eslint.config.mjs
+++ frontend/src/eslint.config.mjs
@@ -1,6 +1,7 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
+import eslintConfigPrettier from "eslint-config-prettier";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -10,7 +11,13 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript", "prettier"),
+  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  // Prettierとの競合を防ぐ設定を追加
+  {
+    rules: {
+      ...eslintConfigPrettier.rules, // prettierConfigからルールを追加
+    },
+  },
 ];
 
 export default eslintConfig;
