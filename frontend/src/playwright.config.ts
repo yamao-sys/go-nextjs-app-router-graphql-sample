88796a61191646e5fc398d46cf67fd139759fe25
yamao-sys <yamaopanku@gmail.com>
2024-12-20 11:14:53 +0900

    CIのE2Eのデバッグ: URLのみ指定

--- frontend/src/playwright.config.ts
+++ frontend/src/playwright.config.ts
@@ -82,7 +82,6 @@ export default defineConfig({
   /* Run your local dev server before starting the tests */
   webServer: {
     command: 'npm run dev',
-    port: 3001,
     url: baseURL,
     reuseExistingServer: !process.env.CI,
   },
